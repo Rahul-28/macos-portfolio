4b9ea14021a77d47dc6fd281151a070f7a232aaa
Rahul-28 <rahulrajeshrr365@gmail.com>
2025-11-24 21:24:12 +0530

    add first window based on hoc - terminal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { Dock, Navbar, Welcome } from "#components"
+import { Draggable } from "gsap/Draggable";
+import gsap from "gsap";
 
+import { Dock, Navbar, Welcome } from "#components"
+import { Terminal } from "#windows";
+
+gsap.registerPlugin(Draggable);
 const App = () => {
   return (
     <main>
       <Navbar />
       <Welcome />
       <Dock />
+
+      <Terminal />
     </main>
   )
 }
