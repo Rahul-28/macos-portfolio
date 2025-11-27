2707e588773c0e0cd69959c1808aaaa4956c0829
Rahul-28 <rahulrajeshrr365@gmail.com>
2025-11-28 00:02:58 +0530

    impl all windows

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import dayjs from "dayjs"
 
 import { navIcons, navLinks } from "#constants"
+import useWindowStore from "#store/window";
 
 const Navbar = () => {
+
+    const { openWindow } = useWindowStore();
+
     return (
         <nav>
             <div>
@@ -10,8 +14,10 @@ const Navbar = () => {
                 <p className="font-bold">Rahul's Portfolio</p>
 
                 <ul>
-                    {navLinks.map(({ id, name }) => (
-                        <li key={id}>{name}</li>
+                    {navLinks.map(({ id, name, type }) => (
+                        <li key={id} onClick={() => openWindow(type)}>
+                            <p>{name}</p>
+                        </li>
                     ))}
                 </ul>
             </div>
