908bdf46d1cb14d63aa3000d44a5d6d257bbc95c
Rahul-28 <rahulrajeshrr365@gmail.com>
2025-11-28 00:03:41 +0530

    impl all windows part2

---
+++ src/windows/Image.jsx
@@ -0,0 +1,34 @@
+import { WindowControls } from "#components";
+import WindowWrapper from "#hoc/WindowWrapper";
+import useWindowStore from "#store/window";
+
+const Image = () => {
+    const { windows } = useWindowStore();
+    const data = windows.imgfile?.data;
+
+    if(!data) return null;
+
+    const { name, imageUrl } = data
+
+    return <>
+        <div id="window-header">
+            <WindowControls target="imgfile" />
+        </div>
+
+        <div className="p-5 bg-white">
+            { imageUrl ? (
+                <div>
+                    <img 
+                        src={imageUrl} 
+                        alt={name} 
+                        className="w-full h-auto max-h-[70vh] object-contain rounded"
+                    />
+                </div>
+            ) : null}
+        </div>
+    </>
+}
+
+const ImageWindow = WindowWrapper(Image, "imgfile");
+
+export default ImageWindow
